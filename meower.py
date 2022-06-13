1e33d04405f017421b9863f03f81c8652059e5db
Mwalters75 <pancake@mdwalters.ml>
2022-06-13 14:17:56 -0400

    Add files

---
+++ meower.py
@@ -0,0 +1,11 @@
+from ast import Pass
+from requests import get
+import json
+
+def repair():
+    status = get("https://api.meower.org/status").text
+    try:
+        load = json.loads(status)
+        return load["isRepairMode"]
+    except json.decoder.JSONDecodeError:
+        pass
