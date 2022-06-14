6304b54af05351912363b241c9d88d36fcb85b1f
Mwalters75 <pancake@mdwalters.ml>
2022-06-14 13:46:39 -0400

    Let's not make it a 429er

--- meowed.py
+++ meowed.py
@@ -1,11 +1,5 @@
 # Demo Meower client using meower.py
 import meower
 
-page = 1
-meower.change_page(1)
-
-while(meower.page_len() != page):
-	for i in range(0, meower.home_len()):
-		print(meower.get_post(meower.post_id(i)))
-	page += 1
-	meower.change_page(page)
+for i in range(0, meower.home_len()):
+	print(meower.get_post(meower.post_id(i)))
