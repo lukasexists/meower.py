234b339d7fd8dde5a0d54b290ba2cd3374ce422e
Mwalters75 <pancake@mdwalters.ml>
2022-06-14 09:17:38 -0400

    AAAA

--- meower.py
+++ meower.py
@@ -1,6 +1,8 @@
 from requests import get
 import json
 
+page = 1
+
 def repair():
     status = get("https://api.meower.org/status").text
     try:
@@ -10,8 +12,7 @@ def repair():
         pass
 
 def post_id(id):
-    home = get("https://api.meower.org/home").text
-    post_num = (id - 1)
+    home = get(f"https://api.meower.org/home?page={page}").text
     try:
         load = json.loads(home)
         return load["index"][id]
@@ -19,7 +20,7 @@ def post_id(id):
         pass
 
 def home():
-    home = get("https://api.meower.org/home").text
+    home = get(f"https://api.meower.org/home?page={page}").text
     try:
         load = json.loads(home)
         return load["index"]
@@ -27,7 +28,7 @@ def home():
         pass
 
 def home_len():
-    home = get("https://api.meower.org/home").text
+    home = get(f"https://api.meower.org/home?page={page}").text
     try:
         load = json.loads(home)
         return len(load["index"])
@@ -41,3 +42,22 @@ def get_post(id):
         return load["u"] + ": " + load["p"]
     except json.decoder.JSONDecodeError:
         pass
+
+def page_len():
+    home = get(f"https://api.meower.org/home?page={page}").text
+    try:
+        load = json.loads(home)
+        return load["pages"]
+    except json.decoder.JSONDecodeError:
+        pass
+
+def current_page():
+    home = get(f"https://api.meower.org/home?page={page}").text
+    try:
+        load = json.loads(home)
+        return load["page#"]
+    except json.decoder.JSONDecodeError:
+        pass
+
+def change_page(page_num):
+    page = page_num
